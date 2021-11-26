da6602d9f17ef62a65c0fd4bbf923049c65553fa
Cdukedev <cdukedev@gmail.com>
2021-11-26 13:54:11 -0500

    added comments to help with the code

--- gameMenu.py
+++ gameMenu.py
@@ -1,2 +1,10 @@
+# Display game menu
+# Try to get user selection
+#     If user selection is 1
+#             Return 1
+#     If user selection is 2
+#             Return 2    
+#     If user selection is 3
+#             Return 3
+# if user imput is invalid, display error message
 def gameMenu():
-    
