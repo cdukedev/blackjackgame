73077963734a67e5a4bf85a6e7553fcecac30ced
Cdukedev <cdukedev@gmail.com>
2021-11-28 18:45:15 -0500

    made changes to pseudo code and brought all functions into the main file

--- vingtEtUn.py
+++ vingtEtUn.py
@@ -14,7 +14,7 @@
 #                      If user selection is 3
 #                               Return 3
 #                  If user selection is invalid, display error message and try again
-#           2. Define DisplayPause function
+#           2. Define printPause function
 #                   Display message
 #                   Pause before next message
 #           3. Define getUserName function
@@ -28,7 +28,9 @@
 #                  elif user input equals stay
 #                        return False 
 #           5. Define playerTurn function
-#                   randomly generated number is assigned to diceOne
+#                   if userChoice equals false
+#                        break from function
+#                   dice one is equal to diceRoll function
 #                   diceOne is removed from the list
 #                   if receivedTotal is equal to or greater than 14
 #                        sendTotal is equal to startTotal + diceOne
@@ -40,85 +42,78 @@
 #                        sendTotal is equal to receivedTotal + totalRoll
 #                        return sendTotal
 #           6. Define ComputerTurn function
-#                   randomly generated number is assigned to diceoOne
+#                   if computer total is greater that 17 
+#                           break from function
+#                   randomly generated number is assigned to diceOne
 #                   if computerTotal is greater than or equal to 14
 #                          computerTotal is equal to computerTotal + diceOne
 #                   elif computerTotal is less than 14 
 #                         randomly generated number is assigned to dice two
 #                         totalRoll is the sum of dice one and dice two 
 #                         computerTotal is equal to computerTotal + totalRoll
-                        NEEDS WORK
+
+
 #           7. Define determineWinLoss function
 #               if userTotal is greater than 21
 #                  Display userName loses 
-#                  
+#                  gameMenu is called
 #               elif computerTotal is greater than 21
 #                  Display userName wins
+#                  gameMenu is called
 #               elif usertotal is equal to 21 and computerTotal is equal to 21
 #                  Display userName and computerName tie
+#                  gameMenu is called
 #               elif userTotal is equal to 21
 #                  Display userName wins
+#                  gameMenu is called
 #               elif computerTotal is equal to 21
 #                  Display Computer hit 21
-#                  Display user loses
-#               
+#                  Display userName loses
+#                  gameMenu is called
+#               if userInput equals stay and computerTotal is greater than 17
+#                       If userTotal is greater than computerTotal
+#                               Display userName wins
+#                               gameMenu is called
+#                       If userTotal is less than computerTotal
+#                               Display userName loses
+#                               gameMenu is called
+#                       If userTotal is equal to computerTotal
+#                               Display userName and computerName tie
+#                               gameMenu is called
+#              Else:
+#                    break from function
 
-                  
-                        NEEDS WORK
-#           8. Define Main function 
-#                     Go into a loop of rounds where player & house will take turns throwing the dice.
-#                     At each turn the system will ask the player/house whether to stay or roll.
-#                     While userOne is not stay or user One is not bust and computer is not stay or computer is not bust
-#                           If the house has a running total of 17 or higher, it stays, and the turn passes to the player.
-#                           If the player/house option is to roll, the program simulates the roll of the two dies and updates & displays the player’s running total.
-#                     If the player/house starts the round with an accumulated total of 14 points or more, only one die will be rolled.
-#                     Once the game is over the system will display the result
-#                     Winner
-#                     Loser
-#                     Tie]
-                        NEEDS WORK
+
 #           9. Define main function 
 #                     Declare userTotal and computerTotal as 0
 #                     Declare userName as empty string
-#                     Display game menu 
+#                     Go to game menu function
 #                         Return Choice
 #                     numberSet is assigned to a list equal to 1 decks of cards 
 #                     If choice is 1
 #                          enter gameRules function
 #                     Else if choice is 2
-#                           Deal first two cards to player and computer
-#                                 Cards are removed from the list
-#                           determineWinLoss function
-#                           Display player and computer cards
+#                           determineWinLoss function passing userTotal, computerTotal
 #                                 hitOrStay function
-#                                 While user input is not equal to stay
+#                                 While user input is not equal to stay 
 #                                       playerTurn function with userTotal as parameter
 #                                       computerTurn function with computerTotal as parameter
 #                                       display player and computers New totals
+#                                       determineWinLoss function passing userTotal, computerTotal
 #                                       hitOrStay function
-#
-#
-#                           
-#                           
+#                                 While computer total is less 17
+#                                       computerTurn function with computerTotal as parameter
+#                                       determineWinLoss function passing userTotal, computerTotal
 #                     Else if choice is 3
 #                          Exit the game with a message
 #
-
-
-
-
-
-
-
-
-
 #           10. Define gameRules function
 #                     Display game rules
+#
 #           11. Define playAgain function
 #                     try to prompt user to play again
 #                             if user input equals yes
-#                                   numberSet is set back to original values
-#                                   return numberSet
+#                                   return True
 #                             elif user input equals no
 #                                   display exit message
 #                                   exit program    
@@ -126,103 +121,107 @@
 #output:
 
 import random
-from playAgain  import playAgain
-from DisplayPause import DisplayPause
-from gameRules import gameRules
-from getUserName import getUserName
-from hitOrStay import hitOrStay
-from rollDice import rollDice
-from computerTurn import computerTurn
-from calculateScore import calculateScore
-from gameMenu import gameMenu
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###List of numbers to be used in the game equal to two decks of cards###
-numberSet = [,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,11,11,11,11]
-
-###roll dice then remove from list###
-diceRoll = random.choice(numberSet)
-numberSet.remove(diceRoll)
-
-
-
-# Project Requirements
-# The program will start by prompting the user for the name of the player.
-
-# The game will be implemented using a menu-driven interface that provides the following options:
-
-# 1. See the Rules.
-# 2. Play Vingt-et-un.
-# 3. Quit.
-# If the user chooses 1 from the menu, display the rules of the game.
-
-# If the user chooses 2 from the menu
-
-# Go into a loop of rounds where player & house will take turns      throwing the dice.
-
-# At each turn the system will ask the player/house whether to stay or roll.
-
-# If the house has a running total of 17 or higher, it stays, and the turn passes to the player.
-# If the player/house option is to roll, the program simulates the roll of the two dies and updates & displays the player’s running total.
-# If the player/house starts the round with an accumulated total of 14 points or more, only one die will be rolled.
-# Once the game is over the system will display the result
-
-# Winner
-# Loser
-# Tie
-
-
-# If the user chooses 3 from the menu, display a good-bye message and quit.
-
-# The users can play the game as many times as they wish until they choose 3 to Quit
-
-
-
-
-
-
-
-
-
-
-
+import time
+
+def playerTurn():
+
+def computerTurn():
+
+def gameMenu():
+
+def getUserName():
+
+def hitOrStay():
+  
+def gameRules():
+
+def determineWinLoss(userTotal, computerTotal):
+
+def printPause(fname):
+    print(fname)
+    time.sleep(.25)
+
+def playAgain():
+    while True:
+        try:
+            response = input("Would you like to play again (y/n)? :").lower()
+            if response == "y":
+                return True
+            elif response == "n":
+                printPause("\nThank you for playing!\n")
+                printPause("\nGoodbye!\n")
+                exit()
+            
+        except TypeError:
+            print("Invalid response. Try again.")
+        except ValueError:
+            print("Invalid response. Try again.")
+        except:
+            print("Invalid response. Try again.")
+
+def diceRoll(dice):
+    rollDice = random.choice(dice)
+    return rollDice
+#Define main function 
+def main():
+
+# Declare Variables
+    userTotal = 0
+    userName = getUserName()
+    computerTotal = 0
+    userChoice = 0
+
+# Go to game menu function
+    choice = gameMenu()
+
+# numberSet is assigned to a list equal to 1 decks of cards 
+    numberSet = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,11,11,11,11]
+
+# Determine choices and perform actions related to choices
+    if choice == 1:
+        gameRules()
+    elif choice == 2:
+    #Game starts with initial rolls
+        userDice1 = diceRoll(numberSet)
+        numberSet.remove(userDice1)
+        userDice2 = diceRoll(numberSet)
+        numberSet.remove(userDice2)
+        userTotal = userDice1 + userDice2
+        computerDice1 = diceRoll(numberSet)
+        numberSet.remove(computerDice1)
+        computerDice2 = diceRoll(numberSet)
+        numberSet.remove(computerDice2)
+        computerTotal = computerDice1 + computerDice2
+        #Display user and computer totals
+        print(userName + "'s total is", userTotal) 
+        print("Computer total:", computerTotal)
+
+# determineWinLoss function passing userTotal, computerTotal
+        determineWinLoses(userTotal, computerTotal)
+
+# hitOrStay function
+        hitStay = hitOrStay()
+
+# While user input is not equal to stay
+        while hitStay == True
+
+# playerTurn function with userTotal as parameter
+            userTotal = playerTurn(userTotal)
+
+# computerTurn function with computerTotal as parameter
+            computerTotal = computerTurn(computerTotal)
+
+# display player and computers New totals
+            printPause("Your total is: " + str(userTotal))
+            printPause("Computer total is: " + str(computerTotal))
+
+# determineWinLoses function passing userTotal, computerTotal
+            determineWinLoses(userTotal, computerTotal)
+            hitStay = hitOrStay()
+#while computer total is less 17
+        while computerTotal < 17:
+            computerTotal = computerTurn(computerTotal)
+            determineWinLoses(userTotal, computerTotal)
 
 
 
